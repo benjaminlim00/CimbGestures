fe8962209c72099ba4d356905e596cc9f3ce0c59
benzjamin0 <36101433+benjaminlim00@users.noreply.github.com>
2018-12-07 16:15:37 +0800

    second commit

--- app/src/main/java/com/example/benjamin/cimbgestures/RegistrationActivity.java
+++ app/src/main/java/com/example/benjamin/cimbgestures/RegistrationActivity.java
@@ -7,7 +7,7 @@ public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registration);
     }
 
 }
