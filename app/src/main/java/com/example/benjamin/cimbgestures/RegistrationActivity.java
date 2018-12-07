73078f0acea4a59668b7cee7aa4d6c71ff5f2b73
edathr <eda_tan@mymail.sutd.edu.sg>
2018-12-08 02:31:15 +0800

    Added Firebase functionality to the login, DO NOT PUSH YOUR GOOGLE SERVICES JSON!

--- app/src/main/java/com/example/benjamin/cimbgestures/RegistrationActivity.java
+++ app/src/main/java/com/example/benjamin/cimbgestures/RegistrationActivity.java
@@ -5,30 +5,82 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class RegistrationActivity extends AppCompatActivity {
-//    private static final String NUMBER_OF_TIMES_RUN_KEY = "NUMBER_OF_TIMES_RUN_KEY";
-//
-//    int defaultValue = 0;
+    private EditText inputUsername, inputPassword;
+    private Button exitButton, registerButton;
+    DatabaseReference mRootDatabaseRef = FirebaseDatabase.getInstance().getReference();
+    //StorageReference storageReference = FirebaseStorage.getInstance().getReference();
 
+    DatabaseReference databaseReferencePart1;
+
+    String CHILD_NODE_PART1 = "Users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_activity);
-        Button regButton = findViewById(R.id.regButton);
 
-        regButton.setOnClickListener(new View.OnClickListener() {
+        inputUsername = findViewById(R.id.inputUsername);
+        inputPassword = findViewById(R.id.inputPassword);
+
+        exitButton = findViewById(R.id.exitButton);
+        registerButton = findViewById(R.id.regButton);
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(RegistrationActivity.this, RegisterGesture.class);
-                startActivity(i);
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
 
+        databaseReferencePart1 = mRootDatabaseRef.child(CHILD_NODE_PART1);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = inputUsername.getText().toString().trim();
+                String password = inputPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(username)) {
+                    Toast.makeText(RegistrationActivity.this, "Please enter email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(RegistrationActivity.this, "Please enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                try {
+                    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+                    byte[] encodedhash = digest.digest(
+                            password.getBytes(StandardCharsets.UTF_8));
+                    password = new String(encodedhash);
+                    databaseReferencePart1.child(username).child("password").setValue(String.valueOf(password));
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+
+
+                startActivity(new Intent(RegistrationActivity.this, RegisterGesture.class));
             }
         });
     }
+}
     /*
     @Override
     protected void onResume() {
@@ -47,4 +99,3 @@ public class RegistrationActivity extends AppCompatActivity {
 //        startActivity(i);
 
 //    }
-}
