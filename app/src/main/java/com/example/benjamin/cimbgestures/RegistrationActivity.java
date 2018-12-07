76c0f6c24b49a5e65e81eb2d6310dd242713db92
benzjamin0 <36101433+benjaminlim00@users.noreply.github.com>
2018-12-07 17:39:18 +0800

    third commit

--- app/src/main/java/com/example/benjamin/cimbgestures/RegistrationActivity.java
+++ app/src/main/java/com/example/benjamin/cimbgestures/RegistrationActivity.java
@@ -1,13 +1,26 @@
 package com.example.benjamin.cimbgestures;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        setContentView(R.layout.registration_activity);
+        Button regButton = findViewById(R.id.regButton);
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegistrationActivity.this, RegisterGesture.class);
+                startActivity(i);
+
+            }
+        });
     }
 
 }
